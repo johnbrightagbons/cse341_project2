a40c7aa123f1cb7ce5d9526017c560f47f42b105
John Bright Agbons <brightinojohnny@gmail.com>
2025-04-10 16:59:26 +0100

    Correction to Middleware Validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,6 +1,6 @@
 // Data validation middleware Students
 const isValidated = (req, res, next) => {
-  const { name, age, grade } = req.body;
+  const { name, age } = req.body;
 
   if (!name || typeof name !== "string" || name.trim() === "") {
     return res.status(400).json({ error: "Invalid or missing 'name' field" });
@@ -11,30 +11,26 @@ const isValidated = (req, res, next) => {
     return res.status(400).json({ error: "Invalid or missing 'age' field" });
   }
 
-  if (!grade || typeof grade !== "string" || grade.trim() === "") {
-    return res.status(400).json({ error: "Invalid or missing 'grade' field" });
-  }
-
   req.body.age = parsedAge; // Ensure `age` is stored as a number
   next(); // Proceed to the next middleware or route handler
 };
 
 // Data validation middleware for Teaachers
 const validateTeacher = (req, res, next) => {
-  const { name, subject, experience } = req.body;
+  const { name, courseAssigned } = req.body;
   if (!name || typeof name !== "string" || name.trim() === "") {
     return res.status(400).json({ error: "Invalid or missing 'name' field" });
   }
-  if (!subject || typeof subject !== "string" || subject.trim() === "") {
+  if (
+    !courseAssigned ||
+    typeof courseAssigned !== "string" ||
+    courseAssigned.trim() === ""
+  ) {
     return res
       .status(400)
-      .json({ error: "Invalid or missing 'subject' field" });
+      .json({ error: "Invalid or missing 'courseAssigned' field" });
   }
-  if (!experience || typeof experience !== "number" || experience < 0) {
-    return res
-      .status(400)
-      .json({ error: "Invalid or missing 'experience' field" });
-  }
+
   next(); // Proceed to the next middleware or route handler
 };
 
